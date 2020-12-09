b68a775b7b1aaf5c9351e12784a44504315afc8d
HallowenWolf <75751676+HallowenWolf@users.noreply.github.com>
2020-12-09 21:36:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client;
+const {token, prefix} = require("./botconfig.json");
 
 bot.on("ready", async => {
  console.log(`Бот под именем ${bot.user.tag} запущен`)
 });
 
-bot.login("токен")
+bot.login(token)
